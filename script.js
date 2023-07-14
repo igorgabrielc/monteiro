d3bd3184d1fb0361e1d2a46de79b8f4a8c15dc30
igorgabrielc <igor_gabrielc@hotmail.com>
2023-07-14 13:30:26 -0300

    main

--- script.js
+++ script.js
@@ -15,75 +15,32 @@ function toggleMode() {
    }
  }
  
- var slideIndex = 0;
- var slides = document.getElementsByClassName("slide");
- var bars = document.getElementsByClassName("bar");
- 
- function showSlide(index) {
-     // Ocultar todos os slides e desmarcar todos os botões de navegação
-     for (var i = 0; i < slides.length; i++) {
-         slides[i].style.display = "none";
-         bars[i].classList.remove("active");
+ const carrusel = document.querySelector(".carrusel-items");
+
+ let maxScrollLeft = carrusel.scrollWidth - carrusel.clientWidth;
+ let intervalo = null;
+ let step = 1;
+ const start = () => {
+   intervalo = setInterval(function () {
+     carrusel.scrollLeft = carrusel.scrollLeft + step;
+     if (carrusel.scrollLeft === maxScrollLeft) {
+       step = step * -1;
+     } else if (carrusel.scrollLeft === 0) {
+       step = step * -1;
      }
+   }, 10);
+ };
  
-     // Exibir o slide atual e marcar o botão de navegação correspondente
-     slides[index].style.display = "block";
-     bars[index].classList.add("active");
- }
+ const stop = () => {
+   clearInterval(intervalo);
+ };
  
- function nextSlide() {
-     slideIndex++;
-     if (slideIndex >= slides.length) {
-         slideIndex = 0;
-     }
-     showSlide(slideIndex);
- }
+ carrusel.addEventListener("mouseover", () => {
+   stop();
+ });
  
- function previousSlide() {
-     slideIndex--;
-     if (slideIndex < 0) {
-         slideIndex = slides.length - 1;
-     }
-     showSlide(slideIndex);
- }
+ carrusel.addEventListener("mouseout", () => {
+   start();
+ });
  
- // Adicione os seguintes eventos para os botões de navegação
- for (var i = 0; i < bars.length; i++) {
-     bars[i].addEventListener("click", function() {
-         slideIndex = Array.prototype.indexOf.call(bars, this);
-         showSlide(slideIndex);
-     });
- }
- 
- // Adicione os seguintes eventos para os botões de avançar e voltar
- var nextButton = document.getElementById("nextButton");
- var previousButton = document.getElementById("previousButton");
- 
- nextButton.addEventListener("click", nextSlide);
- previousButton.addEventListener("click", previousSlide);
- 
- // Chame a função showSlide para exibir o primeiro slide ao carregar a página
- showSlide(slideIndex);
-
- document.addEventListener("DOMContentLoaded", function(event) {
-  var slides = document.querySelectorAll(".slide");
-  var currentSlide = 0;
-
-  function showSlide(n) {
-    slides[currentSlide].classList.remove("active");
-    currentSlide = (n + slides.length) % slides.length;
-    slides[currentSlide].classList.add("active");
-  }
-
-  function nextSlide() {
-    showSlide(currentSlide + 1);
-  }
-
-  function previousSlide() {
-    showSlide(currentSlide - 1);
-  }
-
-  setInterval(nextSlide, 5000); // Altere o tempo em milissegundos (5000 = 5 segundos)
-
-  showSlide(currentSlide);
-});
+ start();
